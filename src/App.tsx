d4860d4410f237c22d89b89163b13b88b8bed02b
adhokshjavali2005 <adhokshjavali2005@gmail.com>
2026-04-29 00:15:57 +0530

    fix: move toast notifications to top-center

--- src/App.tsx
+++ src/App.tsx
@@ -36,7 +36,7 @@ const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Toaster />
-      <Sonner />
+      <Sonner position="top-center" />
       <BrowserRouter>
         <AuthProvider>
           <CartProvider>
